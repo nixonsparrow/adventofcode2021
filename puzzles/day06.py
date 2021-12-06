474e1e04d7fc5863cffa7b27cc91f31d769844c5
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-06 10:44:22 +0100

    Day 6 part1, not optimum solution for part2

--- puzzles/day06.py
+++ puzzles/day06.py
@@ -1,12 +1,45 @@
-from .methods import txt_opener            
+from .methods import txt_opener
+
+
+class Lanternfish:
+    fish = []
+
+    def __init__(self, days):
+        self.cycle = days
+        Lanternfish.fish.append(self)
+
+    def live_another_day(self):
+        if not self.cycle:
+            self.create_new_lanternfish()
+        else:
+            self.cycle -= 1
+
+    def create_new_lanternfish(self, days=8):
+        self.cycle = 6
+        Lanternfish(days)
 
 
 def part1(input_file):
-    final_input = txt_opener(input_file, '\n')
-    return final_input
+    final_input = txt_opener(input_file, ',')
+    Lanternfish.fish = [Lanternfish(fishy) for fishy in final_input]
+    for day in range(80):
+        for fishy in Lanternfish.fish.copy():
+            fishy.live_another_day()
+    return len(Lanternfish.fish)
 
 
 def part2(input_file):
-    final_input = txt_opener(input_file, '\n')
-    return final_input
+    # final_input = txt_opener(input_file, ',')
+    #
+    # Lanternfish.fish = [Lanternfish(fishy) for fishy in final_input]
+    #
+    # x = [8, 16, 24]
+    # for y in x:
+    #     Lanternfish.fish = []
+    #     for day in range(8):
+    #         for fishy in Lanternfish.fish.copy():
+    #             fishy.live_another_day()
+    #     print(len(Lanternfish.fish))
+
+    return None
             
--- tests/test_day06.py
+++ tests/test_day06.py
@@ -2,14 +2,13 @@ from ..puzzles import day06
 import pytest            
 
 
-@pytest.mark.ignored
 class TestDay06:
 
     def test_part1(self):
-        assert not day06.part1('/../inputs/day06_test.txt')
-        assert not day06.part1('/../inputs/day06_final.txt')
+        assert day06.part1('/../inputs/day06_test.txt') == 5934
+        assert day06.part1('/../inputs/day06_final.txt') == 365131
 
     def test_part2(self):
-        assert not day06.part2('/../inputs/day06_test.txt')
-        assert not day06.part2('/../inputs/day06_final.txt')
+        assert day06.part2('/../inputs/day06_test.txt') == 26984457539
+        # assert day06.part2('/../inputs/day06_final.txt') == 26984457539
             
