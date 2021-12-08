70a337196fe612b9b0886b39e89594474fdd92eb
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-08 10:24:52 +0100

    Day 8 with tests

--- puzzles/day08.py
+++ puzzles/day08.py
@@ -1,12 +1,78 @@
-from .methods import txt_opener            
+from .methods import txt_opener
+
+
+def get_same_bars(a_bars, b_bars):
+    return sum([1 for x in a_bars for y in b_bars if x == y])
+
+
+def get_digit(sequence, dictionary):
+    for digit, v in dictionary.items():
+        print(sequence, v)
+        if get_same_bars(v, sequence) == len(v) and get_same_bars(v, sequence) == len(sequence):
+            print('SAME!', sequence, v)
+            return digit
+
+
+def dictionary_writer(x_digits, digits):
+    for x_digit in x_digits.split():
+        if x_digit not in digits.values():      # exclude digits already known
+            if len(x_digit) == 5:               # search for 2/3/5
+                if get_same_bars(x_digit, digits['9']) == 4:
+                    digits['2'] = x_digit
+                else:
+                    if get_same_bars(x_digit, digits['7']) == 3:
+                        digits['3'] = x_digit
+                    else:
+                        digits['5'] = x_digit
+            else:                               # search for 6/9/0
+                if get_same_bars(x_digit, digits['7']) == 2:
+                    digits['6'] = x_digit
+                else:
+                    if get_same_bars(x_digit, digits['4']) == 4:
+                        digits['9'] = x_digit
+                    else:
+                        digits['0'] = x_digit
+    return digits
+
+
+def get_four_digit_code(x_digits, x_code):
+    digits = {str(digit): '' for digit in range(10)}
+    # check for obvious digit codes
+    for x_digit in x_digits.split():
+        if len(x_digit) == 2:
+            digits['1'] = x_digit
+        elif len(x_digit) == 3:
+            digits['7'] = x_digit
+        elif len(x_digit) == 4:
+            digits['4'] = x_digit
+        elif len(x_digit) == 7:
+            digits['8'] = x_digit
+
+    # now check for rest digits based on previous data
+    while '' in digits.values():
+        digits = dictionary_writer(x_digits, digits)
+
+    code = ''
+    for x in x_code.split():
+        code += get_digit(x, digits)
+
+    return code
 
 
 def part1(input_file):
     final_input = txt_opener(input_file, '\n')
-    return final_input
+
+    # exercise is too simple to spread it
+    # just get total count of sequences with 2, 3, 4 or 7 chars on the right side of input
+    return sum([sum([1 if len(digit) in [2, 3, 4, 7] else 0 for digit in row.split(' | ')[1].split()]) for row in final_input])
 
 
 def part2(input_file):
     final_input = txt_opener(input_file, '\n')
-    return final_input
+    four_digit_codes = []
+    for row in final_input:
+        digits, code = row.split(' | ')
+        four_digit_codes.append(get_four_digit_code(digits, code))
+
+    return sum([int(code) for code in four_digit_codes])
             
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -2,14 +2,48 @@ from ..puzzles import day08
 import pytest            
 
 
-@pytest.mark.ignored
 class TestDay08:
+    dict = {
+        '1': 'cf',
+        '2': 'acdeg',
+        '3': 'acdfg',
+        '4': 'bcdf',
+        '5': 'abdfg',
+        '6': 'abdefg',
+        '7': 'acf',
+        '8': 'abcdefg',
+        '9': 'abcdfg',
+        '0': 'abcefg'
+    }
+
+    def test_get_same_bars(self):
+        assert day08.get_same_bars('abcd', 'bcda') == 4
+        assert day08.get_same_bars('abcd', 'bcda') == len('abcd')
+        assert day08.get_same_bars('abch', 'bcda') == 3
+        assert day08.get_same_bars('abcdefghij', 'ightfkosw') == 4
+
+    def test_get_four_digit_code(self):
+        assert day08.get_four_digit_code('acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab',
+                                         'cdfeb fcadb cdfeb cdbaf') == '5353'
+
+    def test_get_digit(self):
+        assert day08.get_digit('fc', self.dict) == '1'
+        assert day08.get_digit('dcaeg', self.dict) == '2'
+        assert day08.get_digit('dcfga', self.dict) == '3'
+        assert day08.get_digit('dbcf', self.dict) == '4'
+        assert day08.get_digit('gfdba', self.dict) == '5'
+        assert day08.get_digit('gefdab', self.dict) == '6'
+        assert day08.get_digit('cfa', self.dict) == '7'
+        assert day08.get_digit('acfgedb', self.dict) == '8'
+        assert day08.get_digit('bcdfga', self.dict) == '9'
+        assert day08.get_digit('fegcab', self.dict) == '0'
 
     def test_part1(self):
-        assert not day08.part1('/../inputs/day08_test.txt')
-        assert not day08.part1('/../inputs/day08_final.txt')
+        assert day08.part1('/../inputs/day08_test.txt') == 26
+        assert day08.part1('/../inputs/day08_final.txt') == 554
 
+    # @pytest.mark.skip
     def test_part2(self):
-        assert not day08.part2('/../inputs/day08_test.txt')
-        assert not day08.part2('/../inputs/day08_final.txt')
+        assert day08.part2('/../inputs/day08_test.txt') == 61229
+        assert day08.part2('/../inputs/day08_final.txt') == 990964
             
