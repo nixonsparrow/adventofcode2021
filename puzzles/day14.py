172cf86654262c87f59ac4d96254d3ea921c0b2d
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-14 20:48:02 +0100

    Day 14 part 1 | part 2 need new approach

--- puzzles/day14.py
+++ puzzles/day14.py
@@ -1,11 +1,46 @@
 from .methods import txt_opener
+import re
+import string
 
 
-def part1(input_file):
+def polymer_insertion(template, ins_rules):
+    new_template = ''
+    for i in range(len(template)):
+        new_template += template[i]
+        if template[i:i + 2] in ins_rules.keys():
+            new_template += [x for x in ins_rules.values() if x == ins_rules[template[i:i + 2]]][0]
+
+    return new_template
+
+
+def get_occurrences(template):      # returned example: {'A': 256, 'B': 112, 'C': 49}
+    return sorted([{'letter': let, 'total': len(re.findall(let, template))} for let in string.ascii_uppercase if let in template],
+                  key=lambda i: i['total'], reverse=True)
+
+
+def part1(input_file):              # slow calculations are enough
     final_input = txt_opener(input_file, '\n')
-    return final_input
+    template = final_input[0]
+    ins_rules = {row.split(' -> ')[0]: row.split(' -> ')[1] for row in final_input[1:]}
+
+    # get polymer recreated 10 times
+    for _ in range(10):
+        template = polymer_insertion(template, ins_rules)
+
+    # subtract: most frequent - least frequent character
+    letters = get_occurrences(template)
+    return letters[0]['total'] - letters[-1]['total']
 
 
-def part2(input_file):
+def part2(input_file):              # fast calculations needed
     final_input = txt_opener(input_file, '\n')
-    return final_input
+    template = final_input[0]
+    ins_rules = {row.split(' -> ')[0]: row.split(' -> ')[1] for row in final_input[1:]}
+
+    # get polymer recreated 10 times
+    for _ in range(40):
+        template = polymer_insertion(template, ins_rules)
+
+    # subtract: most frequent - least frequent character
+    letters = get_occurrences(template)
+    return letters[0]['total'] - letters[-1]['total']
--- tests/test_day14.py
+++ tests/test_day14.py
@@ -2,13 +2,22 @@ from ..puzzles import day14
 import pytest
 
 
-# class TestDay14:
-# 
-#     def test_part1(self):
-#         assert not day14.part1('/../inputs/day14_test.txt')
-#         assert not day14.part1('/../inputs/day14_final.txt')
-# 
-#     def test_part2(self):
-#         assert not day14.part2('/../inputs/day14_test.txt')
-#         assert not day14.part2('/../inputs/day14_final.txt')
-            
+class TestDay14:
+    def test_polymer_insertion(self):
+        assert day14.polymer_insertion('ABCD', {'AB': 'E', 'CD': 'F'}) == 'AEBCFD'
+
+    def test_get_occurrences(self):
+        assert day14.get_occurrences('AAAA') == [{'letter': 'A', 'total': 4}]
+        assert day14.get_occurrences('CAABBDAACCBBA') == [
+            {'letter': 'A', 'total': 5}, {'letter': 'B', 'total': 4},
+            {'letter': 'C', 'total': 3}, {'letter': 'D', 'total': 1},
+        ]
+
+    def test_part1(self):
+        assert day14.part1('/../inputs/day14_test.txt') == 1588
+        assert day14.part1('/../inputs/day14_final.txt') == 2447
+
+    @pytest.mark.skip
+    def test_part2(self):
+        assert day14.part2('/../inputs/day14_test.txt') == 2188189693529
+        assert day14.part2('/../inputs/day14_final.txt') == 1
