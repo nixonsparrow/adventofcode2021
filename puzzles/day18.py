4ef650f69b34ba2818eda00e1ea2f910bb195ce0
Nikodem Wróbel <nixonsparrow@gmail.com>
2021-12-18 23:39:02 +0100

    Day18 special testing to find best way

--- puzzles/day18.py
+++ puzzles/day18.py
@@ -1,9 +1,117 @@
+import re
 from .methods import txt_opener
+
+
+def sum_nests(nest_a, nest_b):
+    return f'[{nest_a},{nest_b}]'
+
+
+def explode_index(nest):
+    ex_index = 0
+    opened_brackets = 0
+
+    for i in range(len(nest)):
+        if nest[i] == '[':
+            opened_brackets += 1
+            if opened_brackets == 5:
+                return ex_index
+        elif nest[i] == ']':
+            opened_brackets -= 1
+
+        ex_index += 1
+    return False
+
+
+def explode_nest(nest):
+    i = explode_index(nest)
+
+    nrs = re.findall(r'\d+', nest[i:i + 5])
+    if len(nrs) < 2: nrs = re.findall(r'\d+', nest[i:i + 6])
+    # l_db = True if len(nrs[0]) > 1 else False
+    # r_db = True if len(nrs[1]) > 1 else False
+    print(i, nrs)
+    left_nr, right_nr = None, None
+
+    l_i = i
+    while l_i > 0:
+        try:
+            left_nr = int(nest[l_i])
+            break
+        except ValueError: pass
+        l_i -= 1
+
+    r_i = i + 5
+    while r_i < len(nest) - 1:
+        try:
+            right_nr = int(nest[r_i])
+            break
+        except ValueError: pass
+        r_i += 1
+
+    print(nest)
+    if left_nr is not None:
+        # l_i_after = l_i + 2 if len(nrs[0]) > 1 else l_i + 1
+        nest = f'{nest[:l_i]}{str(int(nrs[0]) + left_nr)}{nest[l_i + 1:]}'
+    print(nest)
+    if right_nr is not None:
+        # r_i_after = r_i + 2 if len(nrs[0]) > 1 else r_i + 1
+        nest = f'{nest[:r_i]}{str(int(nrs[1]) + right_nr)}{nest[r_i + 1:]}'
+    print(nest)
+
+    x = 5
+    for nr in nrs:
+        x += 1 if len(nr) > 1 else 0
+
+    nest = nest[:i] + nest[i + x:]
+    print(nest)
+
+    nest = nest.replace(',]', ',0]')
+    nest = nest.replace('[,', '[0,')
+    print(nest)
+    # while '[]' in nest:
+    #     nest.replace('[]', '')
+    # while ',,' in nest:
+    #     nest.replace(',,', ',')
+
+    return nest
+
+
+def split_index(nest):
+    for nr in re.findall(r'\d\d', nest):
+        return nest.index(nr)
+
+    return False
+
+
+def split_nest(nest):
+    i = split_index(nest)
+
+    nr = int(nest[i:i + 2])
+    splitted = nest[:i] + f'[{nr // 2 if nr % 2 == 0 else nr // 2 + 1},{nr//2}]' + nest[i + 2:]
+    print('Splitted:', splitted)
+    return splitted
 
 
 def part1(input_file):
     final_input = txt_opener(input_file, '\n')
-    return final_input
+    nest = final_input[0]
+    final_input = final_input[1:]
+    for row in final_input:
+        print('pre_row  ', nest)
+        nest = sum_nests(nest, row)
+        print('after_row', nest)
+        while explode_index(nest) or split_index(nest):
+            print('found split:', split_index(nest))
+            while split_index(nest):
+                nest = split_nest(nest) if split_index(nest) else nest
+                continue
+            while explode_index(nest):
+                print('pre explode   ', nest)
+                nest = explode_nest(nest) if explode_index(nest) else nest
+                print('after explode ', nest)
+
+            print()
+    return nest
 
 
 def part2(input_file):
--- tests/test_day18.py
+++ tests/test_day18.py
@@ -2,13 +2,48 @@ from ..puzzles import day18
 import pytest
 
 
-# class TestDay18:
-# 
-#     def test_part1(self):
-#         assert not day18.part1('/../inputs/day18_test.txt')
-#         assert not day18.part1('/../inputs/day18_final.txt')
-# 
-#     def test_part2(self):
-#         assert not day18.part2('/../inputs/day18_test.txt')
-#         assert not day18.part2('/../inputs/day18_final.txt')
-            
+class TestDay18:
+    nests_ex = ['[[[[[9,8],1],2],3],4]', '[7,[6,[5,[4,[3,2]]]]]', '[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]',
+             '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]']
+    nests_sp = ['[13,[2,2]]', '[[[2,3],12],[1,3]]']
+
+    def test_sum_nests(self):
+        assert day18.sum_nests('[[2,3],4]', '[2,2]') == '[[[2,3],4],[2,2]]'
+
+    # def test_explode_nest(self):
+        # assert day18.explode_nest(self.nests_ex[0]) == '[[[[0,9],2],3],4]'
+        # assert day18.explode_nest(self.nests_ex[1]) == '[7,[6,[5,[7,0]]]]'
+        # assert day18.explode_nest(self.nests_ex[2]) == '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'
+        # assert day18.explode_nest(self.nests_ex[3]) == '[[3,[2,[8,0]]],[9,[5,[7,0]]]]'
+        #
+
+    def test_special(self):
+        assert day18.explode_nest('[[[[[0,0],2]],[[[[10,5]]],9]') == '[[[[0,2]],[[[[10,5]]],9]'
+        assert day18.explode_nest('[[[[0,2]],[[[[10,5]]],9]') == '[[[[0,2]],[[5,5],5]],9]'
+        assert day18.explode_nest('[[[[0,2]],[[5,5],5]],9]') == 0
+
+    def test_split_nest(self):
+        assert day18.split_nest(self.nests_sp[0]) == '[[7,6],[2,2]]'
+        assert day18.split_nest(self.nests_sp[1]) == '[[[2,3],[6,6]],[1,3]]'
+
+    def test_explode_index(self):
+        assert day18.explode_index(self.nests_ex[0]) == 4
+        assert day18.explode_index(self.nests_ex[1]) == 12
+        assert day18.explode_index(self.nests_ex[2]) == 10
+        assert day18.explode_index(self.nests_ex[3]) == 24
+
+        assert not day18.explode_index(self.nests_sp[0])
+
+    def test_split_index(self):
+        assert day18.split_index(self.nests_sp[0]) == 1
+        assert day18.split_index(self.nests_sp[1]) == 8
+
+        assert not day18.split_index(self.nests_ex[0])
+
+    # def test_part1(self):
+    #     assert day18.part1('/../inputs/day18_test.txt') == '[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]'
+    #     assert not day18.part1('/../inputs/day18_final.txt')
+
+    # def test_part2(self):
+    #     assert not day18.part2('/../inputs/day18_test.txt')
+    #     assert not day18.part2('/../inputs/day18_final.txt')
